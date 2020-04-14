be27145315e30a3356fc2e27ccb2808cafdd4f07
Marius Pașcalău <41474702+MariusPascalau@users.noreply.github.com>
2020-04-14 10:09:44 +0300

    Initial commit

---
+++ src/main/java/isp/lab8/safehome/DoorStatus.java
@@ -0,0 +1,5 @@
+package isp.lab8.safehome;
+
+public enum DoorStatus {
+    OPEN, CLOSE;
+}
---
+++ src/main/java/isp/lab8/safehome/SafeHome.java
@@ -0,0 +1,8 @@
+package isp.lab8.safehome;
+
+public class SafeHome {
+
+    public static void main(String[] args) {
+
+    }
+}
