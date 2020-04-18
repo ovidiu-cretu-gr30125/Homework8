072d2225e5527f00fc11fd901ee391b55820be77
Ovidiu Cretu <cretuovidiudaniel@yahoo.com>
2020-04-18 07:16:03 +0300

    rezolvare problema nume fisier

--- src/main/java/Exercise1/ConsoleUI.java
+++ src/main/java/Exercise1/ConsoleUI.java
@@ -4,11 +4,12 @@ import java.io.File;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+
 public class ConsoleUI {
     public static void main(String [] args) throws CryptoException {
         Scanner scanner = new Scanner(System.in);
         while(true){
-            System.out.println("Type #encrypt# to encrypt a file\n Type #decrypt# to decrypt a file\n Type #exit# to exit.");
+            System.out.println("Type #encrypt# to encrypt a file\nType #decrypt# to decrypt a file\nType #exit# to exit.");
             if(UI(scanner.next(),scanner)) return ;
         }
     }
@@ -18,15 +19,29 @@ public class ConsoleUI {
             case "encrypt":
                 System.out.println("Enter the name of the file you want to encrypt: ");
                 String inputFileName = scanner.next();
+                String inputFileNameWithoutExtension;
+                int index = inputFileName.lastIndexOf('.');
+                if (index == -1) {
+                    inputFileNameWithoutExtension=inputFileName;
+                } else {
+                    inputFileNameWithoutExtension= inputFileName.substring(0, index);
+                }
                 File inputFile = new File(inputFileName);
-                File outputFile = new File("" + inputFileName + ".enc");
+                File outputFile = new File("" + inputFileNameWithoutExtension + ".enc");
                 doCrypt.encryptFile(inputFile, outputFile);
                 break;
             case "decrypt":
                 System.out.println("Enter the name of the file you want to decrypt: ");
                 String inputFileName1 = scanner.next();
+                String inputFileNameWithoutExtension1;
+                int index1 = inputFileName1.lastIndexOf('.');
+                if (index1 == -1) {
+                    inputFileNameWithoutExtension1=inputFileName1;
+                } else {
+                    inputFileNameWithoutExtension1= inputFileName1.substring(0, index1);
+                }
                 File inputFile1 = new File(inputFileName1);
-                File outputFile1 = new File(""+inputFileName1+".dec");
+                File outputFile1 = new File(""+inputFileNameWithoutExtension1+".dec");
                 doCrypt.decryptFile(inputFile1,outputFile1);
                 break;
             case "exit":
